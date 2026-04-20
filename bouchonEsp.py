f8cffe11b93707a1d5aabc1dc08330219476c4c5
C314 <croguennoc.claude@gmail.com>
2026-04-20 10:32:09 +0200

    initialisation socle programme

---
+++ bouchonEsp.py
@@ -0,0 +1,20 @@
+import requests
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Serv(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        print(self.path)
+        self.send_response(200)
+        self.send_header('Content-type','text/plain; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(b"OK")
+        if self.path == '/quit':
+            exit()
+
+# lancement serveur HTTP bouchon
+
+httpd = HTTPServer(('localhost',8080),Serv)
+print("Serveur HTTP sur 8080")
+httpd.serve_forever()
