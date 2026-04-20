83094010be5f5b69c3d3bbe037344d793304463c
C314 <croguennoc.claude@gmail.com>
2026-04-20 12:33:53 +0200

    supression tread http

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ import threading
 import requests
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
+
+# variables
 from dotenv import load_dotenv
 import os
+
+# FPDF
+from fpdf import FPDF
 
 # initialisation des composants
 print("initialisation des objets")
@@ -26,27 +31,40 @@ if isCamera:
     cam.start()
     image = cam.get_image()
 
+class Sequence():
+    no=0
+    def next():
+        no = no+1
+    def prev()
+        no=no-1
+        if no < 0:
+            no=0
+    def value():
+        return no
+
+seq=Sequence()
+
 # serveur HTTP endPoint
 class Serv(BaseHTTPRequestHandler):
 
     def do_GET(self):
+       print(self.path)
        if self.path == '/':
            self.send_response(200)
            self.send_header('Content-type','text/plain; charset=utf-8')
            self.end_headers()
            self.wfile.write(b"OK")
        if self.path == '/quit':
+           self.send_response(200)
+           self.wfile.write(b"OK")
+           self.send_header('Content-type','text/plain; charset=utf-8')
+           self.end_headers()
            my_event = pygame.event.Event(QUIT)
            pygame.event.post(my_event)
-           self.wfile.write(b"OK")
-           self.send_response(200)
 
-def startWebServeur():
-    print("Lancement WebServer")
-    httpd = HTTPServer(('localhost',8081),Serv)
 
-loop_thread = threading.Thread(target=startWebServeur)
-loop_thread.start()
+print("Lancement WebServer")
+httpd = HTTPServer(('localhost',8081),Serv)
 
 print("lancement boucle")
 loop=True
@@ -55,10 +73,12 @@ fenetre = pygame.display.set_mode([1280, 720])
 fenetre.fill((120, 120, 120))
 pygame.display.flip()
 while loop:
+    # gestion des evenements
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             print("keydown")
-            response = requests.get(os.getenv("URI_Convecteur") + "/convecteur/start")
+            response = requests.get(os.getenv("URI_Convecteur") + "/convecteur/start",timeout=1)
+            print(response.text)
             #API ESP pupitre
             #/volt/start
             #/volt/stop
@@ -70,15 +90,13 @@ while loop:
             #/convecteur/run
             #/convecteur/error
             #/relay/<no>/[On|Off]
-
-
-
-
-            print(response.text)
         if event.type == QUIT:
             print("exit")
             loop=False
-    clock.tick(10)
+    
+    clock.tick(30)
+    httpd.handle_request()
 
 pygame.quit()
+
 print("fin normale du progamme")
