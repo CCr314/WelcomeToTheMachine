def100c8adb31af9d3dd83b0af3a4d443519b535
CCr314 <croguennoc.claude@gmail.com>
2026-04-23 23:01:02 +0200

    dev mercredi 22

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from pygame.locals import *
 import threading
 import requests
 
+# http client
+import urllib.request
+
+
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 # variables
@@ -33,43 +37,197 @@ joystick.init()
 # vairables d'environnement
 load_dotenv()
 
-isCamera = os.getenv("ISCAMERA")=="ON"
+isCamera1 = os.getenv("ISCAMERA1")=="ON"
+isCamera2 = os.getenv("ISCAMERA2")=="ON"
+isCamera=isCamera1 or isCamera2
+URI_Convecteur= os.getenv("URI_Convecteur")
+URI_Pupitre=os.getenv("URI_Pupitre")
+
+
 
 if isCamera:
-    cam = pygame.camera.Camera("/dev/video0",(640,480))
-    cam.start()
-    image = cam.get_image()
+    camlist = pygame.camera.list_cameras()
+    print(camlist)
+    if isCamera1:
+        cam1 = pygame.camera.Camera("/dev/video0",(640,480))
+        cam1.start()
+    if isCamera2:
+        cam2 = pygame.camera.Camera("/dev/video2",(640,480))
+        cam2.start()
+
+
+def Neon(no):
+    if no==0:
+        print("eteint Néon")
+        contents = urllib.request.urlopen(URI_Convecteur + "/neon/off").read()
+        print(contents)
+    else:
+        print("alume neon %d",no)
+        contents = urllib.request.urlopen(URI_Convecteur + "/neon/" + str(no) + "/on").read()
+        print(contents)
+        
+def Ventillo(on):
+    if on:
+        print("ventillo on")
+        contents = urllib.request.urlopen(URI_Convecteur + "/ventillo/on").read()
+        print(contents)
+    else:
+        print("ventillo off")
+        contents = urllib.request.urlopen(URI_Convecteur + "/ventillo/off").read()
+        print(contents)
+
+def Voltmetre(action):
+    if action==0:
+        print("arret voltmettre")
+        contents = urllib.request.urlopen(URI_Pupitre + "/voltmetre/stop").read()
+        print(contents)
+    elif action==1:
+        print("demarrage voltmetre")
+        contents = urllib.request.urlopen(URI_Pupitre + "/voltmetre/start").read()
+        print(contents)
+    elif action==2:
+        print("Arret voltmetre")
+        contents = urllib.request.urlopen(URI_Pupitre + "/voltmetre/run").read()
+        print(contents)
+        
+
+def Convecteur(action):
+    if action==0:
+        print("arret Convecteur")
+        contents = urllib.request.urlopen(URI_Convecteur + "/convecteur/stop").read()
+        print(contents)
+    elif action==1:
+        print("demarrage Convecteur")
+        contents = urllib.request.urlopen(URI_Convecteur + "/convecteur/start").read()
+        print(contents)
+    elif action==2:
+        print("Arret Convecteur")
+        contents = urllib.request.urlopen(URI_Convecteur + "/convecteur/run").read()
+        print(contents)
+
+            # neon, Ventillo, Voltmetre, Convecteur, noEvent, video,boucle,image
+actionSequence=[[0,0,0,0,6,None,False,"demarrage.jpg"],  # seq 0
+                [0,0,0,0,1,"THE MACHINE Intro Complete.mp4",True,None],  # seq 1
+                [1,0,1,1,1,None,False,"vide.jpg"],  # seq 2 démarrage
+                [0,0,0,0,1,None,False,"vide.jpg"],  # seq 3
+                [0,0,0,0,1,None,False,"vide.jpg"],  # seq 4
+                [0,0,0,0,1,None,False,"vide.jpg"],  # seq 5
+                [0,0,0,0,1,None,False,"vide.jpg"],  # seq 6
+                [0,0,0,0,1,None,False,"vide.jpg"],  # seq 7
+                [0,0,0,0,1,None,False,"vide.jpg"],  # seq 8
+                [0,0,0,0,1,None,False,"vide.jpg"],  # seq 9
+                [0,0,0,0,1,None,False,"vide.jpg"],  # seq 10
+                [0,0,0,0,1,None,False,"vide.jpg"],  # seq 11
+                [0,0,0,0,1,"Back To The Future - Overture.mp3",False],  # seq 12
+                [0,0,0,0,1,None,False,"vide.jpg"]]  # seq 13
+
 
 class Sequence():
     no=0
-    def next():
-        no = no+1
-    def prev():
-        no=no-1
-        if no < 0:
-            no=0
+    vid=None
+    vidBoucle=False
+    img=None
+    event=0
+    def next(self):
+        self.no = self.no + 1
+        self.action()
+    def prev(self):
+        self.no=self.no-1
+        if self.no < 0:
+            self.no=0
+        self.action()
+    def raz(self):
+        self.no=0
+        self.action()
     def value():
-        return no
+        return self.no
+    def action(self):
+        print("Go sequence %d",self.no)
+        Neon(actionSequence[self.no][0])
+        Ventillo(actionSequence[self.no][1])
+        Voltmetre(actionSequence[self.no][2])
+        Convecteur(actionSequence[self.no][3])
+        self.event=actionSequence[self.no][4]
+        if actionSequence[self.no][5] == None:
+            self.vid=None
+            self.vidBoucle=False
+        else:
+            self.vid=Video("./videos/" + actionSequence[self.no][5])
+            self.vidBoucle=actionSequence[self.no][6]
+        if actionSequence[self.no][7] == None:
+            self.img=None
+        else:
+            self.img=pygame.image.load("./images/" + actionSequence[self.no][7])
 
+        # actions spécifiques       
+        if self.no==8 and isCamera1:  # prise des photos
+            image1 = cam1.get_image()
+            pygame.image.save(image1, "./images/photo1.png")
+
+        if self.no==8 and isCamera2:  # prise des photos
+            image2 = cam2.get_image()
+            pygame.image.save(image2, "./images/photo2.png")
+
+        if self.no==12:
+            impression()
+            
+        #elif self.no==1:
+        #elif self.no==2:
+        #elif self.no==3:
+        #else:
+            
+            
 seq=Sequence()
 
 # serveur HTTP endPoint
 class Serv(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        
        print(self.path)
-       if self.path == '/':
+       retour = True  # par defaut
+       
+       if self.path == '/info':
            self.send_response(200)
+           self.send_header('Access-Control-Allow-Origin', '*')
+           self.send_header('Access-Control-Allow-Methods', '*')
+           self.send_header('Access-Control-Allow-Headers', '*')
+           self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
            self.send_header('Content-type','text/plain; charset=utf-8')
            self.end_headers()
-           self.wfile.write(b"OK")
-       if self.path == '/quit':
-           self.send_response(200)
-           self.wfile.write(b"OK")
-           self.send_header('Content-type','text/plain; charset=utf-8')
-           self.end_headers()
-           my_event = pygame.event.Event(QUIT)
-           pygame.event.post(my_event)
+           self.wfile.write(b"{'seq':'0', 'equipe':'0'}")
+       else:
+           if self.path == '/':
+               print("Ne doit pas arriver")
+               retour=False
+           if self.path == '/quit':
+               retour=True
+               my_event = pygame.event.Event(QUIT)
+               pygame.event.post(my_event)
+           if self.path == '/next':
+                seq.next()
+           if self.path == '/prev':
+                seq.prev()
+           if self.path == '/raz':
+                seq.raz()
+           if retour:
+                self.send_response(200)
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.send_header('Access-Control-Allow-Methods', '*')
+                self.send_header('Access-Control-Allow-Headers', '*')
+                self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
+                self.send_header('Content-type','text/plain; charset=utf-8')
+                self.end_headers()
+                self.wfile.write(b"OK")
+           else:
+                self.send_response(400)
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.send_header('Access-Control-Allow-Methods', '*')
+                self.send_header('Access-Control-Allow-Headers', '*')
+                self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
+                self.send_header('Content-type','text/plain; charset=utf-8')
+                self.end_headers()
+                self.wfile.write(b"KO")
 
 
 def impression():
@@ -79,54 +237,86 @@ def impression():
     pdf.set_font('BTTF', size=12)
     with pdf.rotation(angle=10, x=10, y=10):
         pdf.text(10,10,"Welcome to the Machine")
-
+    if isCamera1:
+        pdf.image("images/photo1.png", x=50, y=60)
     pdf.output("./temp/test.pdf")
 
-print("Lancement WebServer")
-httpd = HTTPServer(('localhost',8081),Serv)
+print("Préparation WebServer")
+def lanceHttpServ():  
+    print("lancement serveur HTTP port 8081")
+    httpd = HTTPServer(('localhost',8081),Serv)
+    httpd.serve_forever()
+
+loop_thread = threading.Thread(target=lanceHttpServ)
+loop_thread.start()
+
+
+def checkEvent(event):
+    value = 0
+    if (event.type == KEYDOWN and event.key == 97) or (event.type == JOYBUTTONDOWN and event.button == 2):
+        value = 1  # choix A
+    if (event.type == KEYDOWN and event.key == 98) or (event.type == JOYBUTTONDOWN and event.button == 0):
+        value = 2 # choix B
+    if (event.type == KEYDOWN and event.key == 99) or (event.type == JOYBUTTONDOWN and event.button == 3):
+        value = 3 # choix C
+    if (event.type == KEYDOWN and event.key == 100) or (event.type == JOYBUTTONDOWN and event.button == 4):
+        value = 4 # choix D
+    if (event.type == KEYDOWN and event.key == 13) or (event.type == JOYBUTTONDOWN and event.button == 1):
+        value = 5 # demarreur on
+    if (event.type == KEYUP and event.key == 13) or (event.type == JOYBUTTONDOWN and event.button == 1):
+        value = 6 # demarreur off
+    if (event.type == KEYDOWN and event.key == 1073741904) or (event.type == JOYAXISMOTION and event.axis == 1 and event.value > 0.5):
+        value = 7 # gauche
+    if (event.type == KEYDOWN and event.key == 1073741903) or (event.type == JOYAXISMOTION and event.axis == 1 and event.value < -0.5):
+        value = 8 # droite
+    if (event.type == KEYDOWN and event.key == 1073741906) or (event.type == JOYAXISMOTION and event.axis == 0 and event.value < -0.5):
+        value = 9 # haut
+    if (event.type == KEYDOWN and event.key == 1073741905) or (event.type == JOYAXISMOTION and event.axis == 0 and event.value > 0.5):
+        value = 10 # bas
+    return value    
+    
 
 print("lancement boucle")
 loop=True
 clock = pygame.time.Clock()
-fenetre = pygame.display.set_mode([1920, 1080])
+if os.getenv("ISFULLSCREEN")=="ON":
+    fenetre = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+else:
+    fenetre = pygame.display.set_mode((1200, 860))
+
 fenetre.fill((120, 120, 120))
 pygame.display.flip()
-impression()
 
-vid = Video("./videos/THE MACHINE Intro Complete.mp4")
+seq.raz()
 
 while loop:
     # gestion des evenements
     for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            print("keydown")
-            response = requests.get(os.getenv("URI_Convecteur") + "/convecteur/start",timeout=1)
-            print(response.text)
-            #API ESP pupitre
-            #/volt/start
-            #/volt/stop
-            #/volt/run
+        eventno=checkEvent(event)
+        if eventno > 0:
+            print(eventno)
+            if seq.event==eventno:
+                seq.next()
 
-            #API convecteur
-            #/convecteur/start
-            #/convecteur/stop
-            #/convecteur/run
-            #/convecteur/error
-            #/relay/<no>/[On|Off]
+        
         if event.type == QUIT:
             print("exit")
             loop=False
 
+        
+
     clock.tick(30)
     pygame.display.update()
-    
-    vid.draw(fenetre, (0,0), force_draw=False)
 
-    if vid.isEnd():
-        vid.close()
-        EnCours = False
-    
-    #httpd.handle_request()
+    if seq.vid != None:
+        seq.vid.draw(fenetre, (0,0), force_draw=False)
+
+        if seq.vid.isEnd():
+            seq.vid.close()
+            EnCours = False
+            
+    if seq.img != None:
+        fenetre.blit(seq.img, (0,0))
 
 pygame.quit()
 
